65b894b9d2bb4242f43385415c2bfd8d977420fa
Marina-sudary <mech694539@gmail.com>
2026-02-18 22:48:44 +0300

    "Добавлен файл simple_test.py"

--- test/simple_test.py
+++ test/simple_test.py
@@ -1,2 +1,36 @@
 def test_just_a():
     assert 2==2
+
+
+
+# pytest_ui_api_template
+
+## Шаблон для автоматизации тестирования на python
+
+### Шаги
+1. Склонировать проект `git clone https://github.com/Marina-sudary/pytest_ui_api_template.git`
+2. Установить зависимости
+3. Запустить тесты `pytest`
+
+### Стек:
+- pytest
+- selenium
+- requests
+- _sqlalchemy_
+- allure
+- config
+
+### Структура;
+- ./test - тесты
+- ./pages - описание страниц
+- ./api - хелперы для работы с API
+- ./db - хелперы для работы с БД
+
+### Полезные ссылки
+- [Подсказка по markdown](https://www.markdownguide.org/basic-syntax/)
+- [Подсказка  по ./gitignore](https://www.toptal.com/developers/gitignore)
+
+### Библиотеки (!)
+- pip install pytest
+- pip install selenium
+- pip install webdriver-manager    
